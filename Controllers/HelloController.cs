9c64b966cdfc0ca2fa1ff661a636f7e05c366c4f
srikanthpragada <srikanthpragada@gmail.com>
2019-03-07 09:33:52 +0530

    07-MAR

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,32 @@
+﻿using aspnetmvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aspnetmvc.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Good Morning!";
+            ViewBag.Title = "ASP.NET MVC";
+            return View();
+        }
+
+        // Hello/Trainer
+        public ActionResult Trainer()
+        {
+            var t = new Trainer { Name = "Srikanth", Email = "srikanthpragada@gmail.com" };
+            t.Skills = new List<String>();
+            t.Skills.Add("MS.NET");
+            t.Skills.Add("Java");
+            t.Skills.Add("Python");
+            t.Skills.Add("ML");
+            return View(t);
+        }
+    }
+}
---
+++ Models/Trainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace aspnetmvc.Models
+{
+    public class Trainer
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public List<String> Skills { get; set; }
+    }
+}
