8c47ba4c0c3edfed7baa850ea5c9b6e0bbd9588c
srikanthpragada <srikanthpragada@gmail.com>
2019-03-08 09:31:16 +0530

    08-MAR

---
+++ Controllers/DiscountController.cs
@@ -0,0 +1,28 @@
+﻿using aspnetmvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aspnetmvc.Controllers
+{
+    public class DiscountController : Controller
+    {
+        // GET: Discount
+        [HttpGet]
+        public ActionResult Index()
+        {
+            Discount model = new Discount();
+            return View(model);
+        }
+
+        // POST : Discount
+        [HttpPost]
+        public ActionResult Index(Discount model)
+        {
+            model.Result = model.Amount * model.Rate / 100; 
+            return View(model);
+        }
+    }
+}
---
+++ Models/Discount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace aspnetmvc.Models
+{
+    public class Discount
+    {
+        [Range(1000,100000,ErrorMessage ="Amount must be between 1000 and 100000")]
+        public double Amount { get; set; }
+
+        [Range(5, 50, ErrorMessage = "Discount rate must be between 5 and 50")]
+        public double Rate  { get; set; }
+
+        public double Result { get; set; }
+
+    }
+}
