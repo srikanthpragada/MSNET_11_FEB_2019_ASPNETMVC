a9bcdad6b573ac9100160ae9d049dba875da244d
srikanthpragada <srikanthpragada@gmail.com>
2019-03-09 09:28:46 +0530

    09-MAR

---
+++ Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using aspnetmvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aspnetmvc.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Home");
+            }
+
+            return View(user);
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace aspnetmvc.Models
+{
+    public class User
+    {
+        [StringLength(10,MinimumLength = 5,ErrorMessage ="Username must be 5 to 10 chars")]
+        [Required]
+        public string Name { get; set; }
+
+        [StringLength(10, MinimumLength = 6, ErrorMessage = "Password must be 6 to 10 chars")]
+        [Required]
+        public string Password { get; set; }
+
+        [Compare("Password",ErrorMessage ="Confirm Password does not match with password")]
+        [Required]
+        public string ConfirmPassword { get; set; }
+
+        [RegularExpression(@"^\d{10}$", ErrorMessage ="Mobile must be 10 digits")]
+        public string Mobile { get; set; }
+
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+    }
+}
