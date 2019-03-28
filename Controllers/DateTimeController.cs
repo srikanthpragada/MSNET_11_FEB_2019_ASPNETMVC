829e0c3ad9b74fb68e40b39dd91daf0ac4e77de1
srikanthpragada <srikanthpragada@gmail.com>
2019-03-28 09:29:21 +0530

    28-MAR

---
+++ Controllers/DateTimeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace aspnetmvc.Controllers
+{
+    public class DateTimeController : ApiController
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return DateTime.Now.ToString(); 
+        }
+
+        [HttpGet]
+        public String Get(int id)
+        {
+            if (id == 1)
+                return DateTime.Today.ToLongDateString();
+            else
+                return DateTime.Now.ToLongTimeString();
+        }
+
+
+    }
+}
